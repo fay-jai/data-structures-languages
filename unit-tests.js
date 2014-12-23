72b91ac709985baf79551d58b77bb64680100fc6
Willson Mock <willson.mock@gmail.com>
2014-12-23 15:19:56 -0500

    Add skeletion for unit-tests.js

---
+++ unit-tests.js
@@ -0,0 +1,28 @@
+(function (_ds) {
+
+  /* 
+   * Linked List
+  */
+
+  /* 
+   * Stack
+  */
+
+  /* 
+   * Queue
+  */
+
+  /* 
+   * Tree
+  */
+
+  /* 
+   * Binary Search Tree
+  */
+
+  /* 
+   * Graph
+  */
+
+
+})(_ds);
