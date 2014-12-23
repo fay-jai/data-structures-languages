b924b35fe80c5c30542faf42d11be69accf829e3
Willson Mock <willson.mock@gmail.com>
2014-12-23 15:14:14 -0500

    Add skeleton functions in data-structures.js

--- data-structures.js
+++ data-structures.js
@@ -1,5 +1,37 @@
 (function (global) {
   var _ds = {};
 
+  var makeNode = function (value) {
+
+  };
+
+  var treeNode = function (value) {
+
+  };
+
+  _ds.linkedList = function () {
+
+  };
+
+  _ds.stack = function () {
+
+  };
+
+  _ds.queue = function () {
+
+  };
+
+  _ds.tree = function () {
+
+  };
+
+  _ds.binarySearchTree = function () {
+
+  };
+
+  _ds.graph = function () {
+
+  };
+
   global._ds = _ds;
 })(window);
