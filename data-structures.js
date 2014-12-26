12d8e621eb36f00b481f550416e942e4c03587d9
Willson Mock <willson.mock@gmail.com>
2014-12-26 12:35:07 -0500

    Add hash table interface in data-structures.js

--- data-structures.js
+++ data-structures.js
@@ -359,6 +359,35 @@
     return bst;
   };
 
+  _ds.hashTable = function (arrayLimit) {
+    var h       = {};
+    var storage = {};
+
+    var hashFunction = function (str, max) {
+      var hash = 0;
+      for (var i = 0; i < str.length; i++) {
+        hash = (hash<<5) + hash + str.charCodeAt(i);
+        hash = hash & hash; // Convert to 32bit integer
+        hash = Math.abs(hash);
+      }
+      return hash % max;
+    };
+
+    h.insert = function (key, value) {
+
+    };
+
+    h.retrieve = function (key) {
+
+    };
+
+    h.remove = function (key) {
+
+    };
+
+    return h;
+  };
+
   _ds.graph = function () {
 
   };
