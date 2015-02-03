8822d9fdec651b3319612167ea3c04fe505b34fb
Willson Mock <willson.mock@gmail.com>
2015-02-03 13:01:34 -0800

    Add implementation for various nodes

--- redo-3.js
+++ redo-3.js
@@ -7,3 +7,33 @@
  * Binary Search Tree
  * Hash Table
 */
+
+var SinglyLinkedListNode = function (value) {
+  return {
+    value : value,
+    next  : null
+  };
+};
+
+var DoublyLinkedListNode = function (value) {
+  return {
+    value : value,
+    next  : null,
+    prev  : null
+  };
+};
+
+var TreeNode = function (value) {
+  return {
+    value    : value,
+    children : []
+  };
+};
+
+var BinaryTreeNode = function (value) {
+  return {
+    value : value,
+    left  : null,
+    right : null
+  };
+};
