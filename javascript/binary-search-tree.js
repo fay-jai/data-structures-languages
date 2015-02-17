fe37b78bf6a6e6ae9836e8cf51ed85d4ced3e4a8
Willson Mock <willson.mock@gmail.com>
2015-02-17 12:02:06 -0800

    Fill out shell for binary search tree

--- javascript/binary-search-tree.js
+++ javascript/binary-search-tree.js
@@ -1,7 +1,95 @@
-var binarySearchTreeNode = function () {
-
+var binarySearchTreeNode = function (value) {
+  return {
+    value : value,
+    left  : null,
+    right : null
+  };
 };
 
 var binarySearchTree = function () {
-
+  var tree  = Object.create( binarySearchTreeMethods );
+  tree.root = null;
+  return tree;
 };
+
+var binarySearchTreeMethods = (function () {
+  var add = function (node) {
+
+  };
+
+  var _add = function (start, node) {
+
+  };
+
+  var remove = function (node) {
+
+  };
+
+  var _remove = function (start, node) {
+
+  };
+
+  var contains = function (node) {
+
+  };
+
+  var _contains = function (start, node) {
+
+  };
+
+  var getMaxNode = function () {
+
+  };
+
+  var _getMaxNode = function (start) {
+
+  };
+
+  var getMinNode = function () {
+
+  };
+
+  var _getMinNode = function (start) {
+
+  };
+
+  var getDepth = function () {
+
+  };
+
+  var _getDepth = function (start) {
+
+  };
+
+  var isEmpty = function () {
+
+  };
+
+  var depthFirstSearch = function () {
+
+  };
+
+  var _depthFirstSearch = function (start, callback) {
+
+  };
+
+  var breadthFirstSearch = function () {
+
+  };
+
+  var _breadFirstSearch = function (start, callback) {
+
+  };
+
+  return {
+    add                : add,
+    remove             : remove,
+    contains           : contains,
+    getMaxNode         : getMaxNode,
+    getMinNode         : getMinNode,
+    getDepth           : getDepth,
+    isEmpty            : isEmpty,
+    depthFirstSearch   : depthFirstSearch,
+    breadthFirstSearch : breadthFirstSearch
+  };
+})();
